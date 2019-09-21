7875d88b5f9aaa5c03fadecaa8281fbb0905a5f4
yellow duck <orangeboi@yandex.ru>
2019-09-21 22:23:26 +0300

    implemented FileSystem module

--- core/FileSystem/FileSystem.cpp
+++ core/FileSystem/FileSystem.cpp
@@ -16,6 +16,27 @@ FileSystem& FileSystem::getInstance() {
     return instance;
 }
 
-int FileSystem::startUP() {}
+int FileSystem::startUP() {
+    /*  find engine base directory path */
+    std::string PathStr = SDL_GetBasePath();
+    size_t pos = PathStr.rfind("bin");
+    PathStr = PathStr.substr(0, pos);
+    const char* PathPtr = PathStr.c_str();
+    basePath = StringID(hashCrc32(PathPtr), PathPtr);
+
+    /*  find hidden appdata directory path */
+    // PathStr = SDL_GetPrefPath("", "");
+    // prefPath = StringID(hashCrc32(PathStr), PathStr);
+    return 0;
+}
 
 int FileSystem::shutDown() {}
+
+bpt::ptree& FileSystem::parseJSON(const char* filename) {
+    bpt::read_json(filename, pt);
+    return pt;
+}
+
+StringID& FileSystem::getBasePath() {   return basePath;    }
+
+// StringID& FileSystem::getPrefPath() {}
--- core/FileSystem/FileSystem.hpp
+++ core/FileSystem/FileSystem.hpp
@@ -10,7 +10,7 @@
     that can manipulate file names and paths,
     open, close, read and write individual files,
     scan contents of a directory and handle
-    asynchronious file I/O requests.
+    asynchronious file I/O requests (future feature).
     Game engine uses SDL File I/O abstraction as
     its core. Moreover, FileSystem module is
     depended on some of the Boost C++ Libraries.
@@ -19,12 +19,25 @@
 */
 
 #pragma once
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include "StringID.hpp"
+#include <SDL.h>
+#include <SDL_image.h>
+namespace bpt = boost::property_tree;
+
 
 class FileSystem {
 private:
+    StringID        basePath;                               /*  engine base dir path                        */
+    // StringID        prefPath;                               /*  hidden appdata dir path                     */
+    bpt::ptree      pt;                                     /*  boost property tree to load and parse files */
     FileSystem();
 public:
-    static FileSystem& getInstance();
-    int startUP();
-    int shutDown();
+    static FileSystem& getInstance();                       /*  get singleton getInstance                   */
+    int startUP();                                          /*  start up the subsystem                      */
+    int shutDown();                                         /*  shut down the subsystem                     */
+    bpt::ptree& parseJSON(const char* filename);            /*  load and parse JSON file                    */
+    StringID& getBasePath();                                /*  get engine base dir path                    */
+    // StringID getPrefPath();                                 /*  get hidden appdata dir path                 */
 };
