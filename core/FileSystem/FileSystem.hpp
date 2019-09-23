3fe9f7c19ef06cc9e8034e380d2e9a3d04217efc
yellow duck <orangeboi@yandex.ru>
2019-09-23 19:40:48 +0300

    formatted comments

--- core/FileSystem/FileSystem.hpp
+++ core/FileSystem/FileSystem.hpp
@@ -30,14 +30,14 @@ namespace bpt = boost::property_tree;
 class FileSystem {
 private:
     StringID        basePath;                               /*  engine base dir path                        */
-    // StringID        prefPath;                               /*  hidden appdata dir path                     */
+    // StringID        prefPath;                            /*  hidden appdata dir path                     */
     bpt::ptree      pt;                                     /*  boost property tree to load and parse files */
     FileSystem();
 public:
     static FileSystem& getInstance();                       /*  get singleton getInstance                   */
-    uint32_t startUP();                                          /*  start up the subsystem                      */
-    uint32_t shutDown();                                         /*  shut down the subsystem                     */
-    const bpt::ptree& parseJSON(const char* filename);            /*  load and parse JSON file                    */
+    uint32_t startUP();                                     /*  start up the subsystem                      */
+    uint32_t shutDown();                                    /*  shut down the subsystem                     */
+    const bpt::ptree& parseJSON(const char* filename);      /*  load and parse JSON file                    */
     const StringID& getBasePath() const;                    /*  get engine base dir path                    */
-    // const StringID& getPrefPath() const;                    /*  get hidden appdata dir path                 */
+    // const StringID& getPrefPath() const;                 /*  get hidden appdata dir path                 */
 };
