9c45a83dad8da16c29bb45db78edac38796574f9
yellow duck <orangeboi@yandex.ru>
2019-10-04 22:32:06 +0300

    added startUp and shutDown routines to StackAlloc

--- core/Allocators/StackAlloc.cpp
+++ core/Allocators/StackAlloc.cpp
@@ -11,7 +11,21 @@
 
 StackAlloc::StackAlloc()  {}
 
-StackAlloc& getInstance() {
+StackAlloc& StackAlloc::getInstance() {
     static StackAlloc instance;
     return instance;
+}
+
+int StackAlloc::startUP(uint32_t LSRSize_, uint32_t poolSize_) {
+    this->LSRSize = LSRSize_;
+    realSize = LSRSize_ + poolSize_;
+    marker = LSRSize_;
+    LSRMarker = 0;
+    pool = new char[realSize];
+    return 0;
+}
+
+int StackAlloc::shutDown() {
+    delete[] pool;
+    return 0;
 }
--- core/Allocators/StackAlloc.hpp
+++ core/Allocators/StackAlloc.hpp
@@ -11,11 +11,18 @@
 */
 
 #pragma once
-
+#include <cstdint>
 
 class StackAlloc {
 private:
+    char*       pool;                                   /*  ptr to begining of the pool                             */
+    uint32_t    marker    = 0;                          /*  shows the top of stack                                  */
+    uint32_t    LSRSize   = 0;                          /*  size of Load&StayResident data                          */
+    uint32_t    LSRMarker = 0;                          /*  marker to top of LSR data                               */
+    uint32_t    realSize  = 0;                          /*  size of LSR + size of pool                              */
     StackAlloc();
 public:
     static StackAlloc& getInstance();
+    int startUP(uint32_t LSRSize_, uint32_t poolSize_); /*  start up routine                                        */
+    int shutDown();                                     /*  shut down routine                                       */
 };
