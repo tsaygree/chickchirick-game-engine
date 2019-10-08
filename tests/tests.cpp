cc7847a173c823d4970bc46af770e4c6c9d211d1
yellow duck <orangeboi@yandex.ru>
2019-10-08 21:03:30 +0300

    commented code in tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,10 +6,14 @@
     Created by <chickchirik> on 08/10/2019.
 
     DESCRIPTION:
-    tests sub-project that runs tests on core modules.
+    tests sub-project based on Catch2 testing
+    framework that runs tests on core modules.
+
 */
 
+/*  Catch2 initialisation   */
 #define CATCH_CONFIG_MAIN
 #include "Catch2.hpp"
 
+/*  include tests below     */
 #include "BlockAllocTests.hpp"
