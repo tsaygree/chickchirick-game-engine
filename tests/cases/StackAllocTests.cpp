e58f54727308d5a55ccc35e35c77205e2646beb8
yellow duck <orangeboi@yandex.ru>
2019-10-09 23:34:34 +0300

    added tests for StackAlloc module

--- tests/cases/StackAllocTests.cpp
+++ tests/cases/StackAllocTests.cpp
@@ -12,3 +12,15 @@
 
 #include "Catch2.hpp"
 #include "StackAlloc.hpp"
+#include "MemoryUnits.hpp"
+
+TEST_CASE("StackAlloc tests", "[StackAlloc]") {
+    StackAlloc& stackPool = StackAlloc::getInstance();
+    stackPool.startUP(100_MiB, 200_MiB);
+    SECTION("Big size") {
+        int*  ptr1 = (int*)stackPool.stalloc(201_MiB);
+        char* ptr2 = (char*)stackPool.LSRalloc(101_MiB);
+        REQUIRE(ptr1 == nullptr);
+        REQUIRE(ptr2 == nullptr);
+    }
+}
