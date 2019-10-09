b6d134007e500f045b63a1d7ec5ffa07bcdd9f21
yellow duck <orangeboi@yandex.ru>
2019-10-09 22:56:03 +0300

    added StringID tests

---
+++ tests/cases/StringIDTests.cpp
@@ -0,0 +1,25 @@
+/*
+
+    StringIDTests.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 09/10/2019.
+
+    DESCRIPTION:
+    Tests for StringID module.
+*/
+
+#include "Catch2.hpp"
+#include "StringID.hpp"
+
+TEST_CASE("StringID module tests", "[StringID]") {
+    SECTION("_sid tests") {
+        static_assert("123456789"_sid == 0xCBF43926, "crc32b hash test");
+        REQUIRE("TEST"_sid == 0xEEEA93B8);
+    }
+    SECTION("StringID tests") {
+        StringID str = SID("SID TEST");
+        REQUIRE(str.getID() == 0xF5BE303C);
+        REQUIRE(str.getStr() == "SID TEST");
+    }
+}
