06ec3f09b759a7f7ac03bd4a1cfdc422f5d50c8e
yellow duck <orangeboi@yandex.ru>
2019-10-13 20:56:54 +0300

    added wrapper for boost Vector

---
+++ core/Containers/Vector/Vector.hpp
@@ -0,0 +1,19 @@
+/*
+
+    Vector.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 13/10/2019.
+
+    DESCRIPTION:
+    Vector definition
+
+*/
+
+#pragma once
+#include <boost/container/vector.hpp>
+namespace bcon = boost::container;
+
+/*  alias for boost vector        */
+template<typename T>
+using Vector = bcon::vector<T>;
