b58754c21fc74534c6b5fe7340f306be39ca291d
yellow duck <orangeboi@yandex.ru>
2019-10-16 23:53:09 +0300

    added unified SDL header

---
+++ core/Includes/SDLInclude.hpp
@@ -0,0 +1,14 @@
+/*
+
+    SDLInclude.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 16/10/2019.
+
+    DESCRIPTION:
+    SDL headers include header.
+*/
+
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
