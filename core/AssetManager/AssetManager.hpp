d3ffc8479347ae67f582adbfc72abd8094aad3b9
yellow duck <orangeboi@yandex.ru>
2019-10-16 22:34:31 +0300

    edited comments

--- core/AssetManager/AssetManager.hpp
+++ core/AssetManager/AssetManager.hpp
@@ -10,7 +10,7 @@
     and offloads resources needed by the game. It ensures that
     only one copy of each unique resource is present in memory at
     any given time. It is also responsible for giving the resources
-    their unique identifuers which are StringIDs.
+    their unique identifiers which are StringIDs.
     AssetManager usses different types of memory allocations to store
     each resource properly in memory.
     NOTE: This class is not thread safe.
