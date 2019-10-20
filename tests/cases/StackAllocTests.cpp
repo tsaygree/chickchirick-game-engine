eae6f77e069d2d1b132dc6247038665a814f31eb
yellow duck <orangeboi@yandex.ru>
2019-10-20 13:32:56 +0300

    added StackAlloc tests for pre-allocated pool

--- tests/cases/StackAllocTests.cpp
+++ tests/cases/StackAllocTests.cpp
@@ -16,27 +16,55 @@
 
 TEST_CASE("StackAlloc tests", "[StackAlloc]") {
     StackAlloc stackPool;
-    stackPool.startUP(100_MiB, 200_MiB);
-    SECTION("Big size") {
-        int*  ptr1 = (int*)stackPool.stalloc(201_MiB);
-        char* ptr2 = (char*)stackPool.LSRalloc(101_MiB);
-        REQUIRE(ptr1 == nullptr);
-        REQUIRE(ptr2 == nullptr);
-    }
-    SECTION("Marker tests") {
-        uint32_t marker = stackPool.getMarker();
-        REQUIRE(marker == 100_MiB);
-        int* ptr = (int*)stackPool.stalloc(sizeof(int));
-        REQUIRE(stackPool.getMarker() - marker == sizeof(int));
-        stackPool.freeToMarker(marker);
-        REQUIRE(stackPool.getMarker() == marker);
-        for (uint8_t it = 1; it != 11; it++) {
-            stackPool.stalloc(it * sizeof(int));
+    SECTION("Outer pool tests") {
+        char* pool = new char[300_MiB];
+        stackPool.startUP(pool, 100_MiB, 200_MiB);
+        SECTION("Big size") {
+            int*  ptr1 = (int*)stackPool.stalloc(201_MiB);
+            char* ptr2 = (char*)stackPool.LSRalloc(101_MiB);
+            REQUIRE(ptr1 == nullptr);
+            REQUIRE(ptr2 == nullptr);
         }
-        uint32_t newMarker = stackPool.getMarker();
-        REQUIRE(newMarker - marker == 220);
-        stackPool.free();
-        newMarker = stackPool.getMarker();
-        REQUIRE(newMarker == marker);
+        SECTION("Marker tests") {
+            uint32_t marker = stackPool.getMarker();
+            REQUIRE(marker == 100_MiB);
+            int* ptr = (int*)stackPool.stalloc(sizeof(int));
+            REQUIRE(stackPool.getMarker() - marker == sizeof(int));
+            stackPool.freeToMarker(marker);
+            REQUIRE(stackPool.getMarker() == marker);
+            for (uint8_t it = 1; it != 11; it++) {
+                stackPool.stalloc(it * sizeof(int));
+            }
+            uint32_t newMarker = stackPool.getMarker();
+            REQUIRE(newMarker - marker == 220);
+            stackPool.free();
+            newMarker = stackPool.getMarker();
+            REQUIRE(newMarker == marker);
+        }
+    }
+    SECTION("Inner pool tests") {
+        stackPool.startUP(100_MiB, 200_MiB);
+        SECTION("Big size") {
+            int*  ptr1 = (int*)stackPool.stalloc(201_MiB);
+            char* ptr2 = (char*)stackPool.LSRalloc(101_MiB);
+            REQUIRE(ptr1 == nullptr);
+            REQUIRE(ptr2 == nullptr);
+        }
+        SECTION("Marker tests") {
+            uint32_t marker = stackPool.getMarker();
+            REQUIRE(marker == 100_MiB);
+            int* ptr = (int*)stackPool.stalloc(sizeof(int));
+            REQUIRE(stackPool.getMarker() - marker == sizeof(int));
+            stackPool.freeToMarker(marker);
+            REQUIRE(stackPool.getMarker() == marker);
+            for (uint8_t it = 1; it != 11; it++) {
+                stackPool.stalloc(it * sizeof(int));
+            }
+            uint32_t newMarker = stackPool.getMarker();
+            REQUIRE(newMarker - marker == 220);
+            stackPool.free();
+            newMarker = stackPool.getMarker();
+            REQUIRE(newMarker == marker);
+        }
     }
 }
