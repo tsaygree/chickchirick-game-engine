58cc869b94221e1df067f47226d0d1a5549b9d28
yellow duck <orangeboi@yandex.ru>
2019-10-20 13:48:28 +0300

    edited comments in StackAlloc header

--- core/Allocators/StackAlloc.hpp
+++ core/Allocators/StackAlloc.hpp
@@ -6,7 +6,17 @@
     Created by <chickchirik> on 03/10/2019.
 
     DESCRIPTION:
-    StackAlloc is a singleton stack memory allocator.
+    StackAlloc is a stack memory allocator that works much like
+    stack does. It allocates memory from top of the pool and
+    deallocates it from top to bottom up to special marker (see below)
+    StackAlloc has two types of initialization:
+        1) initialize and allocate memory
+        2) initialize with pre-allocated memory
+    StackAlloc devides memory pool into two sections. The first
+    one (LSR - Load and Stay Resident) is used for data that is
+    supposed to be in memory during all lifetime of the StackAlloc.
+    The second one is used for different dynamic allocations and can
+    return a special marker that later is used for deallocation.
 
 */
 
@@ -25,8 +35,8 @@ private:
     uint32_t    realSize  = 0;                          /*  size of LSR + size of pool                              */
 public:
     StackAlloc();
-    int startUP(uint32_t LSRSize_, uint32_t poolSize_); /*  start up routine                                        */
-    int startUP(char* sPool, uint32_t LSRSize_, uint32_t poolSize_);
+    int startUP(uint32_t LSRSize_, uint32_t poolSize_); /*  initialize and allocate pool                            */
+    int startUP(char* sPool, uint32_t LSRSize_, uint32_t poolSize_); /* initialize with pre-allocated pool          */
     int shutDown();                                     /*  shut down routine                                       */
     void* stalloc(uint32_t size);                       /*  allocate size bytes on stack                            */
     void* LSRalloc(uint32_t size);                      /*  allocate size bytes on LSR stack                        */
