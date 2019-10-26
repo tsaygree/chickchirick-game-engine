65430a06aafbe762bb5d62e1fed380d3f1eb93fe
yellow duck <orangeboi@yandex.ru>
2019-10-26 23:02:29 +0300

    added Map module

---
+++ core/Asset/Map/Map.cpp
@@ -0,0 +1,13 @@
+/*
+
+    Map.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 26/10/2019.
+
+*/
+
+#include "Map.hpp"
+
+Map::Map()  {}
+Map::~Map() {}
---
+++ core/Asset/Map/Map.hpp
@@ -0,0 +1,21 @@
+/*
+
+    Map.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 26/10/2019.
+
+    DESCRIPTION:
+    Map class to handle level/chunk/sequence in-game layout.
+
+*/
+
+#pragma once
+#include "Asset.hpp"
+
+class Map : public Asset {
+private:
+public:
+    Map();
+    ~Map();
+};
