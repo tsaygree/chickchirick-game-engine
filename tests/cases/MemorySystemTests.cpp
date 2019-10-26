d70d589fe22f4a1128024783e5d4d2f8534ae444
yellow duck <orangeboi@yandex.ru>
2019-10-26 16:54:26 +0300

    added tests for MemorySystem module

--- tests/cases/MemorySystemTests.cpp
+++ tests/cases/MemorySystemTests.cpp
@@ -18,6 +18,8 @@ TEST_CASE("MemorySystem module tests", "[MemorySystem]") {
     SystemManager& systemManager = SystemManager::getInstance();
     systemManager.BigInit();
     MemorySystem& memSys  = MemorySystem::getInstance();
+
+    /*  block tests */
     int* iptr = (int*)memSys.alloc(sizeof(int));
     *iptr = 5;
     REQUIRE(*iptr == 5);
@@ -41,5 +43,24 @@ TEST_CASE("MemorySystem module tests", "[MemorySystem]") {
     memSys.free(ptr);
     void* vptr = memSys.alloc(500_MiB);
     memSys.free(vptr);
+
+    /*  stack tests */
+    iptr = (int*)memSys.stalloc(sizeof(int));
+    *iptr = 5;
+    REQUIRE(*iptr == 5);
+    memSys.stfree();
+    ptr = (TestStruct*)memSys.stalloc(sizeof(TestStruct));
+    ptr->decimal = 3;
+    ptr->floating = 3.1415;
+    ptr->symbol = 'p';
+    ptr->ptr = nullptr;
+
+    REQUIRE(ptr->decimal == 3);
+    REQUIRE(ptr->floating == 3.1415);
+    REQUIRE(ptr->symbol == 'p');
+    REQUIRE(ptr->ptr == nullptr);
+    vptr = memSys.stalloc(500_MiB);
+    REQUIRE(vptr == nullptr);
+    memSys.stfree();
     systemManager.BigShutDown();
 }
