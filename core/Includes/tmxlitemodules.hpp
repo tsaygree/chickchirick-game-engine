6528a3c842a2303bb4ba71f7d25156d6f753ab99
yellow duck <orangeboi@yandex.ru>
2019-10-29 21:27:11 +0200

    added header for tmxlite library

---
+++ core/Includes/tmxlitemodules.hpp
@@ -0,0 +1,24 @@
+/*
+
+    tmxlitemodules.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 29/10/2019.
+
+    DESCRIPTION:
+    Header for tmxlite library.
+
+*/
+
+#pragma once
+
+#include <tmxlite/Map.hpp>
+#include <tmxlite/Layer.hpp>
+#include <tmxlite/Object.hpp>
+#include <tmxlite/ObjectGroup.hpp>
+#include <tmxlite/LayerGroup.hpp>
+#include <tmxlite/ImageLayer.hpp>
+#include <tmxlite/Tileset.hpp>
+#include <tmxlite/Property.hpp>
+#include <tmxlite/Types.hpp>
+#include <tmxlite/FreeFuncs.hpp>
