4739d51daa0761fe67fc307abdbdcd90e48897f5
yellow duck <orangeboi@yandex.ru>
2019-10-31 22:04:17 +0200

    added namespace alias for boost property tree

---
+++ core/Containers/PropertyTree/PropertyTree.hpp
@@ -0,0 +1,16 @@
+/*
+
+    PropertyTree.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 31/10/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+namespace bpt = boost::property_tree;
