0d8033f11f4c6f3529c55a33c52a60d38e974c52
yellow duck <orangeboi@yandex.ru>
2019-10-31 21:01:41 +0200

    added GameLoop module

---
+++ core/GameLoop/GameLoop.cpp
@@ -0,0 +1,13 @@
+/*
+
+    GameLoop.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 31/10/2019.
+
+*/
+
+#include "GameLoop.hpp"
+
+GameLoop::GameLoop()  {}
+GameLoop::~GameLoop() {}
---
+++ core/GameLoop/GameLoop.hpp
@@ -0,0 +1,20 @@
+/*
+
+    GameLoop.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 31/10/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class GameLoop {
+private:
+public:
+    GameLoop();
+    ~GameLoop();
+};
