88172a2c113e77cf9412e4dfcaad777465fe6297
yellow duck <orangeboi@yandex.ru>
2019-11-02 22:03:44 +0200

    added GameObject module

---
+++ core/GameObject/GameObject.cpp
@@ -0,0 +1,13 @@
+/*
+
+    GameObject.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 02/11/2019.
+
+*/
+
+#include "GameObject.hpp"
+
+GameObject::GameObject()  {}
+GameObject::~GameObject() {}
---
+++ core/GameObject/GameObject.hpp
@@ -0,0 +1,20 @@
+/*
+
+    GameObject.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 02/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class GameObject {
+private:
+public:
+    GameObject();
+    ~GameObject();
+};
