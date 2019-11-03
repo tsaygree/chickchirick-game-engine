1424cd826934f6890508401e05f189a15c876484
yellow duck <orangeboi@yandex.ru>
2019-11-03 22:02:44 +0200

    added Command class in InputModule

---
+++ core/InputModule/Command/Command.cpp
@@ -0,0 +1,13 @@
+/*
+
+    Command.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 03/11/2019.
+
+*/
+
+#include "Command.hpp"
+
+Command::Command()  {}
+Command::~Command() {}
---
+++ core/InputModule/Command/Command.hpp
@@ -0,0 +1,20 @@
+/*
+
+    Command.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 03/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class Command {
+private:
+public:
+    Command();
+    ~Command();
+};
