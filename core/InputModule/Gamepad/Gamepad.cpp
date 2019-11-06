dbc897947c9fc507395b9b37356a1bb23bc4ff33
yellow duck <orangeboi@yandex.ru>
2019-11-06 17:41:36 +0200

    added connect/disconnect routines to Gamepad module

--- core/InputModule/Gamepad/Gamepad.cpp
+++ core/InputModule/Gamepad/Gamepad.cpp
@@ -10,4 +10,21 @@
 #include "Gamepad.hpp"
 
 Gamepad::Gamepad()  {}
+
 Gamepad::~Gamepad() {}
+
+uint32_t Gamepad::connect(uint32_t deviceID) {
+    assert(isConnected == false);
+    if (SDL_IsGameController(deviceID)) {
+        controller = SDL_GameControllerOpen(deviceID);
+        SDL_Joystick* joyStick = SDL_GameControllerGetJoystick(controller);
+        id = SDL_JoystickInstanceID(joyStick);
+        isConnected = true;
+    }
+}
+
+uint32_t Gamepad::disconnect() {
+    assert(isConnected == true);
+    SDL_GameControllerClose(controller);
+    isConnected = false;
+}
--- core/InputModule/Gamepad/Gamepad.hpp
+++ core/InputModule/Gamepad/Gamepad.hpp
@@ -10,10 +10,17 @@
 */
 
 #pragma once
+#include "SDLModule.hpp"
+#include <cstdint>
 
 class Gamepad {
 private:
+    SDL_GameController* controller  = nullptr;
+    SDL_JoystickID      id;
+    bool                isConnected = false;
 public:
     Gamepad();
     ~Gamepad();
+    uint32_t connect(uint32_t deviceID);
+    uint32_t disconnect();
 };
