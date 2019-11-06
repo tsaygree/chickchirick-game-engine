bc584c0c6b718f4fff33fa94acd2303361605513
yellow duck <orangeboi@yandex.ru>
2019-11-06 20:57:36 +0200

    added startUp/shutDown routines to InputHandler module

--- core/InputModule/InputHandler/InputHandler.cpp
+++ core/InputModule/InputHandler/InputHandler.cpp
@@ -17,3 +17,25 @@ InputHandler& InputHandler::getInstance() {
     static InputHandler instance;
     return instance;
 }
+
+uint32_t InputHandler::startUP(bpt::ptree& config) {
+    uint32_t maxDevice = config.get<uint32_t>("MaxDeviceCount");
+    deviceRegistry.resize(maxDevice);
+    uint32_t numOfJoysticks = SDL_NumJoysticks();
+    uint32_t deviceID = 0;
+
+    while (deviceID < numOfJoysticks && deviceID < maxDevice) {
+        Gamepad* newController = CAST(Gamepad*, mem.alloc(sizeof(Gamepad)));
+        newController->connect(deviceID);
+        deviceRegistry[deviceID] = newController;
+        deviceID++;
+    }
+}
+
+uint32_t InputHandler::shutDown() {
+    for (Gamepad* pad : deviceRegistry) {
+        pad->disconnect();
+        mem.free(pad);
+    }
+    deviceRegistry.clear();
+}
--- core/InputModule/InputHandler/InputHandler.hpp
+++ core/InputModule/InputHandler/InputHandler.hpp
@@ -10,14 +10,25 @@
 */
 
 #pragma once
-
+#include "MemorySystem.hpp"
+#include "PropertyTree.hpp"
+#include "SDLModule.hpp"
+#include "Gamepad.hpp"
+#include "Vector.hpp"
+#include <cstdint>
+#include <cstdio>
 
 class InputHandler {
 private:
+    MemorySystem&       mem = MemorySystem::getInstance();
+    Vector<Gamepad*>    deviceRegistry;
+    SDL_Event           event;
     InputHandler();
 public:
     InputHandler(InputHandler const&)            = delete;
     InputHandler& operator=(InputHandler const&) = delete;
     static InputHandler& getInstance();
     ~InputHandler();
+    uint32_t startUP(bpt::ptree& config);
+    uint32_t shutDown();
 };
