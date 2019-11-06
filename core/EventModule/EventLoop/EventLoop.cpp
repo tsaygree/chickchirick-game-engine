c0708ea5a5a55442b63662fcd441f853ba0057bb
yellow duck <orangeboi@yandex.ru>
2019-11-06 21:14:18 +0200

    created EventModule, added EventLoop and edited CMakeLists

---
+++ core/EventModule/EventLoop/EventLoop.cpp
@@ -0,0 +1,13 @@
+/*
+
+    EventLoop.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 06/11/2019.
+
+*/
+
+#include "EventLoop.hpp"
+
+EventLoop::EventLoop()  {}
+EventLoop::~EventLoop() {}
---
+++ core/EventModule/EventLoop/EventLoop.hpp
@@ -0,0 +1,20 @@
+/*
+
+    EventLoop.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 06/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class EventLoop {
+private:
+public:
+    EventLoop();
+    ~EventLoop();
+};
