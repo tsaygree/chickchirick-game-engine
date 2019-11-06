8f2c8bfb729148843509aacf4a12e61c703c24fa
yellow duck <orangeboi@yandex.ru>
2019-11-06 15:11:31 +0200

    added Gamepad class to InputModule

---
+++ core/InputModule/Gamepad/Gamepad.cpp
@@ -0,0 +1,13 @@
+/*
+
+    Gamepad.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 06/11/2019.
+
+*/
+
+#include "Gamepad.hpp"
+
+Gamepad::Gamepad()  {}
+Gamepad::~Gamepad() {}
---
+++ core/InputModule/Gamepad/Gamepad.hpp
@@ -0,0 +1,19 @@
+/*
+
+    Gamepad.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 06/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+class Gamepad {
+private:
+public:
+    Gamepad();
+    ~Gamepad();
+};
