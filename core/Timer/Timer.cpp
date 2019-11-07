a8793e257f5e87ae65a595fad9860558b597b36d
yellow duck <orangeboi@yandex.ru>
2019-11-07 12:40:04 +0200

    added Timer module

---
+++ core/Timer/Timer.cpp
@@ -0,0 +1,13 @@
+/*
+
+    Timer.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 07/11/2019.
+
+*/
+
+#include "Timer.hpp"
+
+Timer::Timer()  {}
+Timer::~Timer() {}
---
+++ core/Timer/Timer.hpp
@@ -0,0 +1,20 @@
+/*
+
+    Timer.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 07/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class Timer {
+private:
+public:
+    Timer();
+    ~Timer();
+};
