6e4347f6cfb820ae5db1b24a8c48e642e34557a9
yellow duck <orangeboi@yandex.ru>
2019-11-07 14:40:37 +0200

    added Event module

---
+++ core/EventModule/Event/Event.cpp
@@ -0,0 +1,14 @@
+/*
+
+    Event.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 07/11/2019.
+
+*/
+
+#include "Event.hpp"
+
+Event::Event()  {}
+
+Event::~Event() {}
---
+++ core/EventModule/Event/Event.hpp
@@ -0,0 +1,20 @@
+/*
+
+    Event.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 07/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class Event {
+private:
+public:
+    Event();
+    ~Event();
+};
