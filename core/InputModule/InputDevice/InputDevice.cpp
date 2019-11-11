2e58327b9587f90450a88f63974282a20dc71a2d
yellow duck <orangeboi@yandex.ru>
2019-11-11 20:30:45 +0200

    added InputDevice module interface

---
+++ core/InputModule/InputDevice/InputDevice.cpp
@@ -0,0 +1,13 @@
+/*
+
+    InputDevice.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 11/11/2019.
+
+*/
+
+#include "InputDevice.hpp"
+
+InputDevice::InputDevice()  {}
+InputDevice::~InputDevice() {}
---
+++ core/InputModule/InputDevice/InputDevice.hpp
@@ -0,0 +1,20 @@
+/*
+
+    InputDevice.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 11/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class InputDevice {
+private:
+public:
+    InputDevice();
+    ~InputDevice();
+};
