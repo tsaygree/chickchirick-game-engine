c01115fdc107db5c2a2cc69e2cca24e9d3058b96
yellow duck <orangeboi@yandex.ru>
2019-11-19 19:26:50 +0200

    implemented constructor and destructor in RingBuffer template module

--- core/Containers/RingBuffer/RingBuffer.hpp
+++ core/Containers/RingBuffer/RingBuffer.hpp
@@ -10,11 +10,30 @@
 */
 
 #pragma once
+#include "Vector.hpp"
+#include <cstdint>
 
-
+template <typename T>
 class RingBuffer {
 private:
+    Vector<T>     buffer;
+    uint32_t      bufferSize = 0;
+    uint32_t      head = 0;
+    uint32_t      tail = 0;
 public:
-    RingBuffer();
+    RingBuffer(uint32_t size);
     ~RingBuffer();
 };
+
+template <typename T>
+RingBuffer<T>::RingBuffer(uint32_t size) {
+    bufferSize = size;
+    buffer.resize(size);
+    head = 0;
+    tail = 0;
+}
+
+template <typename T>
+RingBuffer<T>::~RingBuffer() {
+    buffer.clear();
+}
