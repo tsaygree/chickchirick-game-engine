b5bfd46bfd5462f074f38f8d213ed960f776305e
yellow duck <orangeboi@yandex.ru>
2019-11-19 14:06:38 +0200

    added RingBuffer container module

---
+++ core/Containers/RingBuffer/RingBuffer.cpp
@@ -0,0 +1,13 @@
+/*
+
+    RingBuffer.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 19/11/2019.
+
+*/
+
+#include "RingBuffer.hpp"
+
+RingBuffer::RingBuffer()  {}
+RingBuffer::~RingBuffer() {}
---
+++ core/Containers/RingBuffer/RingBuffer.hpp
@@ -0,0 +1,20 @@
+/*
+
+    RingBuffer.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 19/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class RingBuffer {
+private:
+public:
+    RingBuffer();
+    ~RingBuffer();
+};
