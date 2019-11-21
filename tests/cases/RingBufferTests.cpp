62dee7cff8fd19fdab5516a6aa80d7b2a95af4a5
yellow duck <orangeboi@yandex.ru>
2019-11-21 20:43:27 +0200

    added push and pop overflow test for RingBuffer module

--- tests/cases/RingBufferTests.cpp
+++ tests/cases/RingBufferTests.cpp
@@ -31,5 +31,20 @@ TEST_CASE("RingBuffer module tests", "[RingBuffer]") {
                 q.pop();
             }
         }
+        SECTION("RingBuffer overflow") {
+            Vector<int> input  = {1, 2, 3, 4, 5};
+            Vector<int> output = {3, 4, 5, 6, 7};
+            RingBuffer<int> q(5);
+            for (auto& val : input) {
+                q.push(val);
+            }
+            q.push(6);
+            q.push(7);
+            for (auto& val : output) {
+                int num = q.front();
+                REQUIRE(num == val);
+                q.pop();
+            }
+        }
     }
 }
