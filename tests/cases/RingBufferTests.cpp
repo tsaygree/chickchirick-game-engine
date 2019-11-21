89542fe3b1c1ec4f6019e5464e48217ff6961daa
yellow duck <orangeboi@yandex.ru>
2019-11-21 19:21:21 +0200

    added RingBuffer test in Push tests section

--- tests/cases/RingBufferTests.cpp
+++ tests/cases/RingBufferTests.cpp
@@ -16,4 +16,19 @@ TEST_CASE("RingBuffer module tests", "[RingBuffer]") {
     SECTION("Constructor test") {
         RingBuffer<int> queue(32);
     }
+    SECTION("Push tests") {
+        SECTION("RingBuffer fully loaded") {
+            Vector<int> input  = {1, 2, 3, 4, 5};
+            Vector<int> output = {1, 2, 3, 4, 5};
+            RingBuffer<int> q(5);
+            for (auto& val : input) {
+                q.push(val);
+            }
+            for (auto& val : output) {
+                int num = q.front();
+                REQUIRE(num == val);
+                q.pop();
+            }
+        }
+    }
 }
