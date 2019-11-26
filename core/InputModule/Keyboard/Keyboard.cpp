cfb11880667498686fee9907789fda0f42bd0742
yellow duck <orangeboi@yandex.ru>
2019-11-26 19:23:47 +0200

    added Keyboard module in InputModule

---
+++ core/InputModule/Keyboard/Keyboard.cpp
@@ -0,0 +1,13 @@
+/*
+
+    Keyboard.cpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 26/11/2019.
+
+*/
+
+#include "Keyboard.hpp"
+
+Keyboard::Keyboard()  {}
+Keyboard::~Keyboard() {}
---
+++ core/InputModule/Keyboard/Keyboard.hpp
@@ -0,0 +1,20 @@
+/*
+
+    Keyboard.hpp
+    chickchirick-game-engine
+
+    Created by <chickchirik> on 26/11/2019.
+
+    DESCRIPTION:
+
+*/
+
+#pragma once
+
+
+class Keyboard {
+private:
+public:
+    Keyboard();
+    ~Keyboard();
+};
