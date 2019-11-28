e2c12df2ea9dc37310143a31a18e802ae665856a
yellow duck <orangeboi@yandex.ru>
2019-11-28 17:16:48 +0200

    implemented processInput method in InputDevice module

--- core/InputModule/InputDevice/InputDevice.hpp
+++ core/InputModule/InputDevice/InputDevice.hpp
@@ -23,7 +23,7 @@
 
 class InputDevice {
 private:
-    std::function<StringID(SDL_Event&)>  cmdCB;
+    std::function<StringID(SDL_Event&)> cmdCB;
     RingBuffer<StringID> commands{100};
 public:
     enum class DeviceType {
@@ -37,6 +37,6 @@ public:
     ~InputDevice() { commands.clear(); }
     virtual inline DeviceType getType() const = 0;
     inline void setCmdProcCallBack(std::function<StringID(SDL_Event&)> newCmdCB) { cmdCB = newCmdCB; }
-    void processInput(SDL_Event& event) {}
+    inline void processInput(SDL_Event& event) { commands.push(cmdCB(event)); }
     inline  Vector<StringID> popCommands()   { return commands.popAll();  }
 };
