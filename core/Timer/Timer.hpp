f5b94589f19262da6abe960a353db619ce5ce4e6
yellow duck <orangeboi@yandex.ru>
2019-12-02 14:57:03 +0200

    refactored Timer module code and deleted cpp file due to deprecation

--- core/Timer/Timer.hpp
+++ core/Timer/Timer.hpp
@@ -18,9 +18,12 @@ class Timer {
 private:
     uint32_t beginTicks = 0;
 public:
-    Timer();
-    ~Timer();
-    uint32_t getTime();
-    uint32_t reset();
-    uint32_t timePassed();
+    Timer() : beginTicks(SDL_GetTicks()) {}
+    ~Timer() {}
+    uint32_t timePassed() { return SDL_GetTicks() - beginTicks; }
+    uint32_t getTime()    { return SDL_GetTicks();              }
+    uint32_t reset() {
+        beginTicks = SDL_GetTicks();
+        return beginTicks;
+    }
 };
